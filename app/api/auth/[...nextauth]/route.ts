880d9241a3b39eec4c9837ed4d9a46c72effe649
BiMalxMe <journeyxbimal@gmail.com>
2025-08-21 07:33:10 +0545

    Feat : Added Auth Options

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,63 +1,11 @@
-import db from "@/app/db";
-import { Keypair } from "@solana/web3.js";
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import { authConfig } from "@/app/lib/auth";
+import NextAuth from "next-auth"
 
-//for now we are using the signup and signnin with google only
+const handler = NextAuth(authConfig)
 
-const handler = NextAuth({
-  
-    providers: [
-        GoogleProvider({
-            //if not found its empty
-          clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-          clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
-        })
-      ],
-      callbacks :{
-        async signIn({ user, account, profile, email, credentials }) {
+export { handler as GET, handler as POST }
 
-          if(account?.provider === "google") {
-            const email = user.email;
-            if(!email) {
-              return false; 
-            }
-            const userdb =await db.user.findFirst({
-              where: {
-                username: email,
-              },
-            })
-            if(userdb) {
-              return true;
-            }
-            const keypair = Keypair.generate();
-            const publicKey = keypair.publicKey.toBase58();
-            const privateKey = keypair.secretKey;
-            await db.user.create({
-              data: {
-                username : email,
-                provider : "Google",
-                name : profile?.name ?? "",
-                // @ts-ignore
-                profilePic : profile?.picture,
-                solWallet : {
-                  create :{
-                    publicKey : publicKey,
-                    privateKey : privateKey.toString(),
-                  }
-                },
-                nprWallet : {
-                  create : {
-                    balance : 0,
-                  }
-                }
-              }
-            })
-          }
-          return false
-        },
-      }
-      
+console.log({
+    clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
 })
-console.log( "here it is" ,process.env.GOOGLE_CLIENT_ID ,process.env.GOOGLE_CLIENT_SECRET)
-export {handler as POST, handler as GET}
