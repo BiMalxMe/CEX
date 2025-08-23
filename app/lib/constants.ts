96d90d525fda5b35d8ce684283d0a7f7ed2e1aca
BiMalxMe <journeyxbimal@gmail.com>
2025-08-23 22:36:00 +0545

    Feat : Jupyter new Price api

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -1,34 +1,32 @@
+import { Connection } from "@solana/web3.js";
+import axios from "axios";
+import { SUPPORTED_TOKENS } from "./tokens";
 
-export interface TokenDetails {
-    name: string;
-    mint: string;
-    native: boolean;
+let LAST_UPDATED: number | null = null;
+let prices: {[key: string]: {
     price: string;
-    image: string;
-    decimals: number;
+}} = {};
+
+const TOKEN_PRICE_REFRESH_INTERVAL = 60 * 1000; // every 60s
+
+export const connection = new Connection("https://solana-devnet.g.alchemy.com/v2/u-HGjUk-M9HMphshm3_KH");
+
+export async function getSupportedTokens() {
+    if (!LAST_UPDATED || new Date().getTime() - LAST_UPDATED < TOKEN_PRICE_REFRESH_INTERVAL) {
+        try {
+            const response = await axios.get("https://lite-api.jup.ag/price/v3?ids=SOL,USDC,USDT");
+            prices = response.data.data;
+            LAST_UPDATED = new Date().getTime();
+            console.log(prices)
+        } catch(e) {
+            console.log(e);
+        }
+    }
+    return SUPPORTED_TOKENS.map(s => ({
+        ...s,
+        price: prices[s.name].price
+    }))
+
 }
 
-
-export const SUPPORTED_TOKENS: TokenDetails[] = [{
-    name: "SOL",
-    mint: "So11111111111111111111111111111111111111112",
-    native: true,
-    price: "180",
-    image: "https://upload.wikimedia.org/wikipedia/commons/3/34/Solana_cryptocurrency_two.jpg",
-    decimals: 9
-}, {
-    name: "USDC",
-    mint: "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v",
-    native: false,
-    price: "1",
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1vAKYEl0YffTpWSxrqEi_gmUsl-0BuXSKMQ&s",
-    decimals: 6
-}, {
-    name: "USDT",
-    mint: "Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB",
-    native: false,
-    price: "1",
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvSxrpym7ij1Hf6zQOltcDORlrJGyj1kPf3A&s",
-    decimals: 6
-}
-]
+getSupportedTokens();
